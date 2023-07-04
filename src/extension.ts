4f3234536fb8c91450f980194d60da9b77e3a1d6
Iewnfod <shangzhiqiuyue@outlook.com>
2023-07-04 09:51:19 +0800

    Create Repository

---
+++ src/extension.ts
@@ -0,0 +1,15 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
+
+
+let client: LanguageClient;
+
+export function activate(context : ExtensionContext) {
+
+}
