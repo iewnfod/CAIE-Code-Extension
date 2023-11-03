3338830af984d868a5731ecb7c25a189aa69fdb3
lightumcc <jessiezhu@createchstudio.com>
2023-11-03 10:16:15 +0800

    add: auto save when run

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,15 @@ function activate(context) {
     console.log("Extension activated");
     const interpreterPathConfig = vscode.workspace.getConfiguration("myCpcConfig");
     const interpreterPath = interpreterPathConfig.get("interpreterPath") || "cpc";
-    // console.log(interpreterPathConfig);
-    // console.log(interpreterPath);
     executeCurrentFile(context);
     registerUpdate(context);
+
+    function saveCurrentFile() {
+        const { activeTextEditor } = vscode.window;
+        if (activeTextEditor) {
+            activeTextEditor.document.save();
+        }
+    }
 
     function executeCurrentFile(context) {
         const command = 'cpc.run';
@@ -18,6 +23,7 @@ function activate(context) {
                 let fileUri = filePath;
                 const decodedFilePath = fileUri ? decodeURI(fileUri) : undefined;
                 const terminal = vscode.window.createTerminal('CPC Interpreter');
+                saveCurrentFile();
                 terminal.sendText(`${interpreterPath} "${decodedFilePath}"`);
                 terminal.show();
             }
--- src/extension.ts.js
+++ src/extension.ts.js
@@ -4,10 +4,14 @@ function activate(context) {
     console.log("Extension activated");
     const interpreterPathConfig = vscode.workspace.getConfiguration("myCpcConfig");
     const interpreterPath = interpreterPathConfig.get("interpreterPath") || "cpc";
-    // console.log(interpreterPathConfig);
-    // console.log(interpreterPath);
     executeCurrentFile(context);
     registerUpdate(context);
+    function saveCurrentFile() {
+        const { activeTextEditor } = vscode.window;
+        if (activeTextEditor) {
+            activeTextEditor.document.save();
+        }
+    }
     function executeCurrentFile(context) {
         const command = 'cpc.run';
         const editor = vscode.window.activeTextEditor;
@@ -17,6 +21,7 @@ function activate(context) {
                 let fileUri = filePath;
                 const decodedFilePath = fileUri ? decodeURI(fileUri) : undefined;
                 const terminal = vscode.window.createTerminal('CPC Interpreter');
+                saveCurrentFile();
                 terminal.sendText(`${interpreterPath} "${decodedFilePath}"`);
                 terminal.show();
             }
